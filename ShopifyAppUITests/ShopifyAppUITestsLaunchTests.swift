668098cb73f4b94482e5dc8f55e0b6daf4332fe6
RadwaMohamed29 <radwamohamed2979@gmail.com>
2022-05-18 20:04:23 -0700

    Initial Commit

---
+++ ShopifyAppUITests/ShopifyAppUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  ShopifyAppUITestsLaunchTests.swift
+//  ShopifyAppUITests
+//
+//  Created by Radwa on 18/05/2022.
+//
+
+import XCTest
+
+class ShopifyAppUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
