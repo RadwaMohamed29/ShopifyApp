6dc4f2a2222531fe88ba9bc436bfcfe7d8b46b99
MennaElsayed5 <mennaaaaelsayed@gmail.com>
2022-05-23 17:50:59 +0200

    UI For home screen

--- ShopifyApp/AppDelegate/Home/View/HomeViewController.swift
+++ ShopifyApp/AppDelegate/Home/View/HomeViewController.swift
@@ -9,21 +9,87 @@ import UIKit
 
 class HomeViewController: UIViewController {
 
+    @IBOutlet weak var homeTV: UITableView!
     override func viewDidLoad() {
         super.viewDidLoad()
+        setupTableView()
 
         // Do any additional setup after loading the view.
     }
-
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    func setupTableView(){
+        homeTV.register(AbsTableViewCell.Nib(), forCellReuseIdentifier: AbsTableViewCell.identifier)
+        homeTV.register(BrandTableViewCell.Nib(), forCellReuseIdentifier: BrandTableViewCell.identifier)
+        homeTV.delegate = self
+        homeTV.dataSource = self
     }
-    */
+    @IBAction func pressedOnSearchBtn(_ sender: UIBarButtonItem) {
+        
+    }
+    
+    @IBAction func pressedOnFavBtn(_ sender: UIBarButtonItem) {
+        
+    }
+    @IBAction  func pressedOnCartBtn(_ sender: UIBarButtonItem) {
+        
+    }
+}
+extension HomeViewController :UITableViewDelegate, UITableViewDataSource{
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 2
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        var rows = 0
+        switch section{
+        case 0:
+            rows = 1
+        default:
+            rows = 1
+        }
+        return rows
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        switch indexPath.section {
+        case 0:
+            let adsCell = tableView.dequeueReusableCell(withIdentifier: AbsTableViewCell.identifier, for: indexPath)
+            return adsCell
+        default:
+       
+            
+                    let brandCell = tableView.dequeueReusableCell(withIdentifier: BrandTableViewCell.identifier, for: indexPath) as! BrandTableViewCell
+                    return brandCell
+        }
+    }
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        var height = 0.0
+        switch indexPath.section{
+        case 0:
+            height = 210
+        default:
+            height = view.frame.height * 1.5
+        }
+        return height
+    }
 
+    func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        var title = ""
+        switch section{
+        case 0:
+            title = ""
+        default:
+            title = ""
+        }
+        return title
+    }
+    func tableView(_ tableView: UITableView, willDisplayHeaderView view: UIView, forSection section: Int) {
+        let header: UITableViewHeaderFooterView = view as! UITableViewHeaderFooterView
+        header.textLabel?.font = UIFont(name: "Optima", size: 18)
+        header.textLabel?.textAlignment = NSTextAlignment.center
+        header.textLabel?.textColor = UIColor.label
+    }
+
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        homeTV.deselectRow(at: indexPath, animated: false)
+    }
 }
