40c12bcfb46f8807ecc4952f440aa6adc7123318
MennaElsayed5 <mennaaaaelsayed@gmail.com>
2022-06-25 18:42:57 +0200

    handle format code in cartScreen

--- ShopifyApp/Shoping/View/ShoppingCartVC.swift
+++ ShopifyApp/Shoping/View/ShoppingCartVC.swift
@@ -22,6 +22,7 @@ class ShoppingCartVC: UIViewController {
     let userDefualt = Utilities()
     let indicator = NVActivityIndicatorView(frame: .zero, type: .ballRotateChase, color: .label, padding: 0)
     var totalPrice=0.0
+    var flag:Bool = true
     override func viewDidLoad() {
         super.viewDidLoad()
         self.title = "Cart"
@@ -29,42 +30,25 @@ class ShoppingCartVC: UIViewController {
         tableView.delegate = self
         tableView.dataSource = self
         productViewModel = ProductDetailsViewModel(appDelegate: (UIApplication.shared.delegate as? AppDelegate)!)
+        
         if Utilities.utilities.getUserNote() == "0" {
-            self.emptyView.isHidden=false
+           // self.emptyView.isHidden=false
+            getCartProductsFromCoreData()
         }
         else{
+            getCartProductsFromCoreData()
             getItemsDraft()
             UpdateTotalPrice()
             self.emptyView.isHidden=true
         }
-        
+    //    checkConnection()
+        print("cacheCart\(CartProducts.count)")
     }
-
-    override func viewWillDisappear(_ animated: Bool) {
-        modifyCountOfItem()
-        
-    }
-
-     func getItemsDraft(){
-         if Utilities.utilities.getUserNote() != "0" {
-             productViewModel?.getItemsDraftOrder(idDraftOrde: Utilities.utilities.getDraftOrder())
-             productViewModel?.itemDraftOrderObservable.subscribe(on: ConcurrentDispatchQueueScheduler
-                 .init(qos: .background))
-             .observe(on: MainScheduler.asyncInstance)
-             .subscribe{ result in
-                 self.itemList = self.productViewModel!.lineItem
-                 self.tableView.reloadData()
-                 print("get items success ")
-             }.disposed(by: disposeBag)
-         }
-         else {
-             self.emptyView.isHidden=false
-         }
-        }
     override func viewWillAppear(_ animated: Bool) {
         self.navigationController?.setNavigationBarHidden(false, animated: true)
        // UpdateTotalPrice()
-        checkCartIsEmpty()
+        checkConnection()
+  //      checkCartIsEmpty()
     }
     func checkCartIsEmpty(){
         DispatchQueue.main.asyncAfter(deadline: .now()+1.0)
@@ -78,103 +62,28 @@ class ShoppingCartVC: UIViewController {
         }
     
     }
-    func updateCustomerNote(){
-            if userDefualt.isLoggedIn(){
-                    let editCustomer = EditCustomerRequest(id: userDefualt.getCustomerId(), email: userDefualt.getCustomerEmail(), firstName: userDefualt.getCustomerName(), password: "\(userDefualt.getUserPassword())", note: "0")
-                    userDefualt.setUserNote(note: editCustomer.note)
-                    print("iddddddddd\(userDefualt.getDraftOrder())")
-                    print("passwordnooooote\(userDefualt.getUserNote())")
-                    productViewModel?.editCustomer(customer: EditCustomer(customer: editCustomer), customerID: userDefualt.getCustomerId(), completion: { result in
-                        switch result{
-                        case true:
-                            print("note Deleted\(editCustomer.note)")
-                        case false:
-                            print("note can't Deleted")
-                        }
-                        
-                    })
-        }
+    override func viewWillDisappear(_ animated: Bool) {
+        modifyCountOfItem()
         
     }
-     func deleteItemFromCart(indexPath:IndexPath){
-        ///product details check product in cart a'3mleha 3la api
-        print("draftId\(userDefualt.getDraftOrder())")
-        if itemList.count == 1{
-            productViewModel?.deleteDraftOrder(draftOrderID: userDefualt.getDraftOrder())
-            self.emptyView.isHidden=false
-            Utilities.utilities.setDraftOrder(id: 0)
-            updateCustomerNote()
-        }else{
-            if userDefualt.isLoggedIn(){
-                if userDefualt.getUserNote() != "0"{
-                    itemList.remove(at: indexPath.row)
-                    tableView.deleteRows(at: [indexPath], with: .fade)
-                    tableView.reloadData()
-                   
-                    let updateDraftOrder = PutOrderRequestTest(draftOrder: ModifyDraftOrderRequestTest(dratOrderId: Int(userDefualt.getDraftOrder()), lineItems: itemList ))
-                    productViewModel?.editDraftOrder(draftOrder: updateDraftOrder, draftID: userDefualt.getDraftOrder(), completion: { result in
-                        switch result{
-                        case true:
-                            print("update order to api ")
-                            self.UpdateTotalPrice()
-            
-                        case false:
-                            print("error to update in api")
-                        }
-                        
-                    })
-
-                   // self.UpdateTotalPrice()
-                }
+    func checkConnection(){
+        HandelConnection.handelConnection.checkNetworkConnection { [self] isConnected in
+            if isConnected{
+                self.flag = true
+            }
+            else{
+                self.flag = false
+                self.getCartProductsFromCoreData()
+                self.alertWarning(indexPath:[], title: "information", message: "this is last update")
+                self.setTotalPrice()
             }
         }
-         //        do{
-         //            try self.productViewModel?.removeProductFromCart(productID: "\(CartProducts[index].id ?? "1")", completionHandler: { response in
-         //                switch response{
-         //                case true:
-         //                    print("remove from cart")
-         //                   //self.getCartProductsFromCoreData()
-         //                    self.tableView.reloadData()
-         //                    if self.CartProducts.count == 0 {
-         //                        self.emptyView.isHidden=false
-         //                    }
-         //                case false:
-         //                    print("error in remove")
-         //                }
-         //            })
-         //        }
-         //        catch let error{
-         //            print(error.localizedDescription)
-         //        }
     }
 
-    
-    func UpdateTotalPrice(){
-        DispatchQueue.main.asyncAfter(deadline: .now()+1.0)
-        { [self] in
-          totalPrice = 0.0
-            for item in itemList {
-                totalPrice += Double(item.quantity)*(Double(item.price) ?? 0.0)
-                totalLable.text = String(totalPrice)
-        }
-       }
-    }
-    func modifyCountOfItem(){
-        let updateDraftOrder = PutOrderRequestTest(draftOrder: ModifyDraftOrderRequestTest(dratOrderId: Int(Utilities.utilities.getDraftOrder()), lineItems: itemList ))
-        productViewModel?.editDraftOrder(draftOrder: updateDraftOrder, draftID: Utilities.utilities.getDraftOrder(), completion: { result in
-            switch result {
-            case true:
-                print("update order to api ")
-            case false:
-                print("error to update in api")
-            }
-        })
-        
-    }
     func showDeleteAlert(indexPath:IndexPath){
         let alert = UIAlertController(title: "Are you sure?", message: "You will remove this item from the cart", preferredStyle: .alert)
         alert.addAction(UIAlertAction(title: "OK", style: .destructive, handler: { [self] UIAlertAction in
-            self.deleteItemFromCart(indexPath: indexPath)
+                self.deleteItemFromCart(indexPath: indexPath)
           //  self.setTotalPrice()
         }))
         alert.addAction(UIAlertAction(title: "Cancel", style: .default, handler: nil))
@@ -198,8 +107,13 @@ class ShoppingCartVC: UIViewController {
 }
 extension ShoppingCartVC :UITableViewDelegate, UITableViewDataSource{
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return itemList.count
-        
+        if flag == true{
+            return itemList.count
+
+        }
+        else {
+            return CartProducts.count
+        }
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
@@ -207,17 +121,18 @@ extension ShoppingCartVC :UITableViewDelegate, UITableViewDataSource{
         cell.deleteFromBagProducts = {
             self.showDeleteAlert(indexPath: indexPath)
         }
-   self.showActivityIndicator(indicator: self.indicator, startIndicator: false)
-            let item = self.itemList[indexPath.row]
+        if flag == true{    let item = self.itemList[indexPath.row]
             cell.updateUI(item: item)
             var count = self.itemList[indexPath.row].quantity
-        
-        cell.addCount={ [self] in
+            let id = self.itemList[indexPath.row].productID
+
+         cell.addCount={ [self] in
                 count+=1
                 cell.productCount.text = "\(count)"
                 self.itemList[indexPath.row].quantity = count
             self.totalPrice += Double(itemList[indexPath.row].price)!
             self.totalLable.text = String(self.totalPrice)
+           self.updateCount(productID: id, count: count)
              
                     }
                 cell.subCount={
@@ -228,11 +143,33 @@ extension ShoppingCartVC :UITableViewDelegate, UITableViewDataSource{
                         self.itemList[indexPath.row].quantity = count
                         self.totalPrice -= Double(self.itemList[indexPath.row].price)!
                         self.totalLable.text = String(self.totalPrice)
+                        self.updateCount(productID: id, count: count)
+
                     }
-                    else{
-                        self.alertWarning(indexPath: indexPath, title: "warning", message: "can't decrease count of item to zero")
-                    }
+                   
                 }
+            
+        }
+        else {
+                    let url = URL(string: CartProducts[indexPath.row].image!)
+                    let processor = DownsamplingImageProcessor(size: cell.productImage.bounds.size)
+                    cell.productTitle.text=CartProducts[indexPath.row].title
+                    cell.productImage.kf.indicatorType = .activity
+                            cell.productImage.kf.setImage(
+                                with: url,
+                                placeholder: UIImage(named: "placeholderImage"),
+                                options: [
+                                    .processor(processor),
+                                    .scaleFactor(UIScreen.main.scale),
+                                    .transition(.fade(1)),
+                                    .cacheOriginalImage
+                                ])
+                    cell.productCount.text = " \(CartProducts[indexPath.row].count)"
+                    cell.productPrice.text = Shared.formatePrice(priceStr: CartProducts[indexPath.row].price!)
+                    cell.deleteFromBagProducts = {[weak self] in
+                    self?.showDeleteAlert(indexPath: indexPath)
+                    }
+        }
         
         return cell
             
@@ -247,6 +184,7 @@ extension ShoppingCartVC :UITableViewDelegate, UITableViewDataSource{
         tableView.deselectRow(at: indexPath, animated: false)
         let detalisVC = ProductDetailsViewController(nibName: "ProductDetailsViewController", bundle: nil)
         detalisVC.productId = String(itemList[indexPath.row].productID)
+        
         self.navigationController?.pushViewController(detalisVC, animated: true)
     }
     func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
@@ -257,4 +195,168 @@ extension ShoppingCartVC :UITableViewDelegate, UITableViewDataSource{
     
 
 }
+extension ShoppingCartVC{
+  
+    func getItemsDraft(){
+        if Utilities.utilities.getUserNote() != "0" {
+            productViewModel?.getItemsDraftOrder(idDraftOrde: Utilities.utilities.getDraftOrder())
+            productViewModel?.itemDraftOrderObservable.subscribe(on: ConcurrentDispatchQueueScheduler
+                .init(qos: .background))
+            .observe(on: MainScheduler.asyncInstance)
+            .subscribe{ result in
+                self.itemList = self.productViewModel!.lineItem
+                self.tableView.reloadData()
+                print("get items success ")
+            }.disposed(by: disposeBag)
+        }
+        else {
+            self.emptyView.isHidden=false
+        }
+       }
 
+   func updateCustomerNote(){
+           if userDefualt.isLoggedIn(){
+                   let editCustomer = EditCustomerRequest(id: userDefualt.getCustomerId(), email: userDefualt.getCustomerEmail(), firstName: userDefualt.getCustomerName(), password: "\(userDefualt.getUserPassword())", note: "0")
+                   userDefualt.setUserNote(note: editCustomer.note)
+                   print("iddddddddd\(userDefualt.getDraftOrder())")
+                   print("passwordnooooote\(userDefualt.getUserNote())")
+                   productViewModel?.editCustomer(customer: EditCustomer(customer: editCustomer), customerID: userDefualt.getCustomerId(), completion: { result in
+                       switch result{
+                       case true:
+                           print("note Deleted\(editCustomer.note)")
+                       case false:
+                           print("note can't Deleted")
+                       }
+                       
+                   })
+       }
+       
+   }
+    func deleteItemFromCart(indexPath:IndexPath){
+       ///product details check product in cart a'3mleha 3la api
+       print("draftId\(userDefualt.getDraftOrder())")
+       if itemList.count == 1{
+           productViewModel?.deleteDraftOrder(draftOrderID: userDefualt.getDraftOrder())
+           self.emptyView.isHidden=false
+           Utilities.utilities.setDraftOrder(id: 0)
+           updateCustomerNote()
+       }else{
+           if userDefualt.isLoggedIn(){
+               if userDefualt.getUserNote() != "0"{
+                   itemList.remove(at: indexPath.row)
+                   tableView.deleteRows(at: [indexPath], with: .fade)
+                   tableView.reloadData()
+                  
+                   let updateDraftOrder = PutOrderRequestTest(draftOrder: ModifyDraftOrderRequestTest(dratOrderId: Int(userDefualt.getDraftOrder()), lineItems: itemList ))
+                   productViewModel?.editDraftOrder(draftOrder: updateDraftOrder, draftID: userDefualt.getDraftOrder(), completion: { result in
+                       switch result{
+                       case true:
+                           print("update order to api ")
+                           self.UpdateTotalPrice()
+           
+                       case false:
+                           print("error to update in api")
+                       }
+                       
+                   })
+
+                  // self.UpdateTotalPrice()
+               }
+           }
+       }
+   }
+
+   
+   func UpdateTotalPrice(){
+       DispatchQueue.main.asyncAfter(deadline: .now()+1.0)
+       { [self] in
+         totalPrice = 0.0
+           for item in itemList {
+               totalPrice += Double(item.quantity)*(Double(item.price) ?? 0.0)
+               totalLable.text = String(totalPrice)
+       }
+      }
+   }
+   func modifyCountOfItem(){
+       let updateDraftOrder = PutOrderRequestTest(draftOrder: ModifyDraftOrderRequestTest(dratOrderId: Int(Utilities.utilities.getDraftOrder()), lineItems: itemList ))
+       productViewModel?.editDraftOrder(draftOrder: updateDraftOrder, draftID: Utilities.utilities.getDraftOrder(), completion: { result in
+           switch result {
+           case true:
+               print("update order to api ")
+           case false:
+               print("error to update in api")
+           }
+       })
+       
+   }
+}
+extension ShoppingCartVC{
+    func getCartProductsFromCoreData(){
+            do{
+                try  productViewModel?.getAllProductsInCart(completion: { response in
+                    //MARK: LSA M5LST4
+                    switch response{
+                    case true:
+                        print("data retrived successfuly")
+                    case false:
+                        print("data cant't retrieved")
+                    }
+                })
+                
+            }
+            catch let error{
+                print(error.localizedDescription)
+            }
+            CartProducts = (productViewModel?.productsInCart)!
+            tableView.reloadData()
+            
+        }
+        func deleteItemFromCoreData(index:IndexPath){
+            do{
+                try self.productViewModel?.removeProductFromCart(productID: "\(CartProducts[index.row].id ?? "1")", completionHandler: { response in
+                    switch response{
+                    case true:
+                        print("remove from cart")
+                        self.getCartProductsFromCoreData()
+                        self.tableView.reloadData()
+                        if self.CartProducts.count == 0 {
+                            self.emptyView.isHidden=false
+                        }
+                    case false:
+                        print("error in remove")
+                    }
+                })
+            }
+            catch let error{
+                print(error.localizedDescription)
+            }
+        }
+        func setTotalPrice(){
+            do{
+                try productViewModel?.updatePrice(completion: { totalPrice in
+                    guard let totalPrice = totalPrice else { return }
+                    Utilities.utilities.setTotalPrice(totalPrice:self.totalPrice ?? 0)
+                    self.totalLable.text = Shared.formatePrice(priceStr: String(totalPrice))
+                    print(totalPrice)
+                    })
+            }catch let error{
+                print(error.localizedDescription)
+            }
+        }
+       @objc func updateCount(productID : Int , count : Int) {
+            do{
+                try  productViewModel?.updateCount(productID: productID, count: count, completionHandler: { response in
+                    //MARK: LSA M5LST4
+                    switch response{
+                    case true:
+                        print("data updated successfuly")
+                    case false:
+                        print("data cant't update")
+                    }
+                })
+            }
+            catch let error{
+                print(error.localizedDescription)
+            }
+        }
+}
