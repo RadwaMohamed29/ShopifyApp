00112b68bb165bd03537f67617a93eee40e1013f
RadwaMohamed29 <radwamohamed2979@gmail.com>
2022-06-26 21:18:36 -0700

    change splash screen Animation

--- ShopifyApp/Main/View/LaunchViewController.swift
+++ ShopifyApp/Main/View/LaunchViewController.swift
@@ -10,21 +10,27 @@ import Lottie
 
 class LaunchViewController: UIViewController {
 
+    @IBOutlet weak var splashView: UIView!
     override func viewDidLoad() {
         super.viewDidLoad()
+
         let animationView = AnimationView()
-        
-        animationView.animation = Animation.named("LuanchAnimation")
-        //animationView.contentMode = .scaleAspectFit
+
+        animationView.animation = Animation.named("Splash")
+        animationView.contentMode = .scaleAspectFit
         animationView.frame = view.bounds
         animationView.loopMode = .loop
         animationView.layoutMargins = UIEdgeInsets(top: 0, left: 0, bottom: 0, right: 0)
+
         animationView.center = view.center
-       
+
+           
+
         animationView.play()
+        view.sendSubviewToBack(animationView)
         view.addSubview(animationView)
         
-        DispatchQueue.main.asyncAfter(deadline: .now() + 4) { [weak self] in
+        DispatchQueue.main.asyncAfter(deadline: .now() + 3) { [weak self] in
             guard let self = self else {return}
             let a = TabBarViewController(nibName:"TabBarViewController", bundle: nil)
             if !Utilities.utilities.isFirstTimeInApp(){
